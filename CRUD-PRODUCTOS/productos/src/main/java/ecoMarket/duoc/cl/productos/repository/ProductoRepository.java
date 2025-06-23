a76fb5e64f8e427e665c0b4f3efc0b4ee8de8ff5
pLoayza <ped.loayza@duocuc.cl>
2025-06-23 05:43:33 -0400

    added logic to Controller, model, repo and services.

--- CRUD-PRODUCTOS/productos/src/main/java/ecoMarket/duoc/cl/productos/repository/ProductoRepository.java
+++ CRUD-PRODUCTOS/productos/src/main/java/ecoMarket/duoc/cl/productos/repository/ProductoRepository.java
@@ -3,5 +3,5 @@ package ecoMarket.duoc.cl.productos.repository;
 import ecoMarket.duoc.cl.productos.model.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
 }
--- CRUD-PRODUCTOS/productos/src/main/java/ecoMarket/duoc/cl/productos/service/ProductoService.java
+++ CRUD-PRODUCTOS/productos/src/main/java/ecoMarket/duoc/cl/productos/service/ProductoService.java
@@ -1,4 +1,32 @@
 package ecoMarket.duoc.cl.productos.service;
 
+import ecoMarket.duoc.cl.productos.model.Producto;
+import ecoMarket.duoc.cl.productos.repository.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ProductoService {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    public List<Producto> obtenerTodos() {
+        return productoRepository.findAll();
+    }
+
+    public Optional<Producto> obtenerPorId(Long id) {
+        return productoRepository.findById(id);
+    }
+
+    public Producto guardar(Producto producto) {
+        return productoRepository.save(producto);
+    }
+
+    public void eliminar(Long id) {
+        productoRepository.deleteById(id);
+    }
 }
