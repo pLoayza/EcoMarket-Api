9ec502ccb124204c7d6baffed93d5dc8e6e42321
Hernán Vera Cabrera <hernan-fe@hotmail.com>
2025-06-22 20:23:45 -0400

    Api usuarios iniciada

---
+++ CRUD-USUARIO/usuarios/src/main/java/ecoMarket/duoc/cl/usuarios/UsuariosApplication.java
@@ -0,0 +1,13 @@
+package ecoMarket.duoc.cl.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuariosApplication.class, args);
+	}
+
+}
